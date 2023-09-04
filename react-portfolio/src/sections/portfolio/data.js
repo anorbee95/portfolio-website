6815f4e51f1cf40d67ad6970b558f21ee08c5d94
Norbert Augusztin <anorbee95@gmail.com>
2023-09-04 18:20:02 +0200

    add new project details

--- react-portfolio/src/sections/portfolio/data.js
+++ react-portfolio/src/sections/portfolio/data.js
@@ -1,6 +1,6 @@
 import Image1 from '../../assets/project1.PNG'
 import Image2 from '../../assets/project2.PNG'
-import Image3 from '../../assets/project1.PNG'
+import Image3 from '../../assets/project3.PNG'
 
 
 const data = [
@@ -11,7 +11,7 @@ const data = [
         title: "Kilövés (Frontend)",
         desc: "Kiloves.hu is my creation—a React-based website that accompanies my unique board game. It enables users to enjoy QR code-accessible mini-games from the board game and offers seamless Stripe payment processing for board game purchases. This project highlights my proficiency in front-end development and e-commerce integration.",
         demo: 'http://kiloves.hu',
-        github: 'https://github.com/anorbee95/projects/kiloves'
+        github: 'https://github.com/anorbee95/projects/tree/main/kiloves'
     },
     {
         id: 2,
@@ -20,16 +20,16 @@ const data = [
         title: "EdiPedi (Frontend)",
         desc: "Edipedi.hu is a visually stunning platform I crafted to spotlight pedicure services provided by a family member. Developed using React, Yarn, Vite, and React Bootstrap, it's a testament to my front-end skills. Stay tuned for an upcoming scheduling system enhancement, making Edipedi.hu a top choice for pedicure enthusiasts.",
         demo: 'http://edipedi.hu',
-        github: 'https://github.com/anorbee95/projects/edipedi'
+        github: 'https://github.com/anorbee95/projects/tree/main/edipedi'
     },
     {
         id: 3,
         category: 'fullstack',
         image: Image3,
-        title: "Project Title Three (Fullstack)",
+        title: "Fullstack Webshop (Fullstack)",
         desc: "The Fullstack Webshop is a collaborative triumph—a full-fledged e-commerce platform developed by a team of four. We adeptly managed version control with Git, utilized React, Yarn, Vite, and React Bootstrap for the frontend, and employed Express.js, PostgreSQL, Supabase, and JWT token authentication on the backend.",
         demo: 'https://fullstack-webshop.web.app/',
-        github: 'https://github.com/anorbee95'
+        github: 'https://github.com/anorbee95/Webshop-Project'
     }
 ]
 
